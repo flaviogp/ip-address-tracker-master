4860e6a32787dd250746e2ccf3d5ae5731ee6810
FlavioGP <flavio.gpinheiro@hotmail.com>
2024-04-04 11:23:45 -0300

    feat: add set LatLng when toggle ip address

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,32 @@
+import { useEffect, useState } from "react"
+import Header from "./components/Header"
 import Map from "./components/Map"
-import useConnectApi from "./components/hooks/useConnectApi"
+import useConnectApi from "./hooks/useConnectApi"
+import { LatLngExpression } from "leaflet"
 
 
 function App() {
-  const {ipLocalization} = useConnectApi('192.169.1.1')
+  const [latlng, setLatlng] = useState<LatLngExpression>([0,0])
+  const [ipValue, setIpvalue] = useState('')
+
+  const {ipLocalization} = useConnectApi(ipValue)
+  console.log(ipLocalization)
+
+    useEffect(() => {
+      if(ipLocalization){
+        setLatlng([ipLocalization.location.lat, ipLocalization.location.lng])
+      }
+      return;
+    },[ipLocalization])
+
   return (
-    <div className="container">
-      <Map />
-      {ipLocalization && <div>{ipLocalization.location.region}</div>}
+    <div className="w-full h-screen">
+      <Header ipLocalization={ipLocalization} setIpvalue={setIpvalue}/>
+      <Map 
+        latlng={latlng}
+      />
+      <div>
+      </div>
     </div>
   )
 }
